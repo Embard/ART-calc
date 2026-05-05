a6b04119afad0ddad2fc9e6b040422a596081be7
Embard <218176366+Embard@users.noreply.github.com>
2026-05-05 16:03:18 +0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,7 @@
-// Firebase config для LabScheduler
-
 export const firebaseConfig = {
   apiKey: "AIzaSyAgeBzb4HLUmaasbZMKm-Zrh2DKGIp5H4M",
   authDomain: "labscheduler-343bb.firebaseapp.com",
-  databaseURL: "https://console.firebase.google.com/project/labscheduler-343bb/database/labscheduler-343bb-default-rtdb/data/~2F?utm_source=chatgpt.com",
+  databaseURL: "https://labscheduler-343bb-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "labscheduler-343bb",
   storageBucket: "labscheduler-343bb.firebasestorage.app",
   messagingSenderId: "63384567603",
